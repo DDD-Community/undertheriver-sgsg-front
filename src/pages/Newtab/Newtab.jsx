1accf2558d1d45f78227275f19baa9428ff41fe2
siyeon <siyeonleeme@gmail.com>
2021-03-27 23:53:51 +0900

    Add chakra UI Setting

--- src/pages/Newtab/Newtab.jsx
+++ src/pages/Newtab/Newtab.jsx
@@ -2,25 +2,14 @@ import React from 'react';
 import logo from '../../assets/img/logo.svg';
 import './Newtab.css';
 import './Newtab.scss';
+import { Box } from "@chakra-ui/react"
 
 const Newtab = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Newtab/Newtab.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-        <h6>The color of this paragraph is defined using SASS.</h6>
-      </header>
+      <Box bg="tomato" w="100%" p={4} color="white">
+        This is the Box
+      </Box>
     </div>
   );
 };
--- src/pages/Newtab/index.jsx
+++ src/pages/Newtab/index.jsx
@@ -4,6 +4,16 @@ import { render } from 'react-dom';
 import Newtab from './Newtab';
 import './index.css';
 
+import { ChakraProvider } from "@chakra-ui/react"
+
+function App() {
+    return (
+        <ChakraProvider>
+            <App />
+        </ChakraProvider>
+    )
+}
+
 render(<Newtab />, window.document.querySelector('#app-container'));
 
 if (module.hot) module.hot.accept();
