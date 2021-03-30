13aa683e550a8da72ac8127989d39044029417a1
munouse <yunho2006@naver.com>
2021-03-31 01:12:11 +0900

    Feat: router settings

---
+++ src/pages/Newtab/components/Popup.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+/** @jsx jsx */
+import { css, jsx } from '@emotion/react';
+const popupWrapper = css`
+  display: none;
+`;
+const Popup = (props:any) => {
+    return (
+     // <main css={popupWrapper}>
+     <main>
+        test!
+     </main>
+    )
+}
+export default Popup;
