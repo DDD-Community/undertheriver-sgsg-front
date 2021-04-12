977f338584dff682b03f15c46036cf2632f4d41b
siyeon <siyeonleeme@gmail.com>
2021-04-13 05:03:28 +0900

    Feat: add tag component to card and fix minor css

--- src/pages/Newtab/components/Card.jsx
+++ src/pages/Newtab/components/Card.jsx
@@ -18,14 +18,24 @@ import {
 } from '@chakra-ui/react';
 /** @jsx jsx */
 import { css, jsx } from '@emotion/react';
+import Tag from './Tag';
 
 const cardWrapper = css`
-  width: 18.875rem;
-  height: 18.875rem;
-  padding: 0.375rem;
+  padding-top: 100%;
+  box-sizing: border-box;
+  position: relative;
   background: white;
-  box-shadow: 0px 8px 8px rgba(222, 218, 209, 0.5);
+  box-shadow: 0 8px 8px rgba(222, 218, 209, 0.5);
   border-radius: 2px;
+
+  .content {
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    padding: 0.375rem;
+  }
 `;
 
 const innerCardWrapper = css`
@@ -34,63 +44,71 @@ const innerCardWrapper = css`
   border: 1px solid rgba(165, 170, 178, 0.3);
   box-sizing: border-box;
   border-radius: 2px;
-  width: 18.125rem;
-  height: 18.125rem;
-  padding: 1rem;
+  padding-top: 100%;
 `;
 const headerSection = css`
   height: 3rem;
   display: flex;
   justify-content: space-between;
+  padding-left: 1rem;
+  .date {
+    align-self: center;
+  }
 `;
 
 const bodySection = css`
-  height: 13.25rem;
+  height: calc(100% - 4.5rem);
+  padding-left: 1rem;
 `;
 
 const tagSection = css`
   height: 1.5rem;
-  padding-top: 1rem;
-  padding-bottom: 1rem;
+  margin-top: -1rem;
 `;
 
 const Card = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <Box css={cardWrapper}>
-      <div css={innerCardWrapper}>
-        <div css={headerSection}>
-          <span>header</span>
-          <Menu>
-            <MenuButton aria-label="Options" size="xs" variant="outline">
-              menu
-            </MenuButton>
-            <MenuList>
-              <MenuItem>New Window</MenuItem>
-              <MenuItem>Open Closed Tab</MenuItem>
-              <MenuItem>Open File</MenuItem>
-            </MenuList>
-          </Menu>
+      <div className="content">
+        <div css={innerCardWrapper}>
+          <div className="content">
+            <div css={headerSection}>
+              <span className="date">header</span>
+              <Menu>
+                <MenuButton aria-label="Options" size="xs" variant="outline">
+                  menu
+                </MenuButton>
+                <MenuList>
+                  <MenuItem>New Window</MenuItem>
+                  <MenuItem>Open Closed Tab</MenuItem>
+                  <MenuItem>Open File</MenuItem>
+                </MenuList>
+              </Menu>
+            </div>
+            <div css={bodySection} onClick={onOpen}>
+              body
+            </div>
+            <div css={tagSection}>
+              <Tag tagColor={'rgba(45, 165, 215, 0.2)'} textColor={'#2DA5D7'} text={'test'} />
+            </div>
+            <Modal isOpen={isOpen} onClose={onClose}>
+              <ModalOverlay />
+              <ModalContent>
+                <ModalHeader>Modal Title</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                  <p>test</p>
+                </ModalBody>
+                <ModalFooter>
+                  <Button colorScheme="blue" mr={3} onClick={onClose}>
+                    Close
+                  </Button>
+                </ModalFooter>
+              </ModalContent>
+            </Modal>
+          </div>
         </div>
-        <div css={bodySection} onClick={onOpen}>
-          body
-        </div>
-        <div css={tagSection}>tag</div>
-        <Modal isOpen={isOpen} onClose={onClose}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Modal Title</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>
-              <p>test</p>
-            </ModalBody>
-            <ModalFooter>
-              <Button colorScheme="blue" mr={3} onClick={onClose}>
-                Close
-              </Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
       </div>
     </Box>
   );
